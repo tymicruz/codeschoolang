c1fd68f5915b2b3ea87eb75c8555506b27d1ceef
tymicruz <tymicruz@gmail.com>
2015-08-06 16:57:14 -0700

    some update on 8-6-15

--- anglearn/app.js
+++ anglearn/app.js
@@ -1,8 +1,8 @@
 (function(){
 
-	var app = angular.module('pplug', []);
+	var app = angular.module('store', []);
 
-	app.controller('PplugController', function(){ 
+	app.controller('StoreController', function(){ 
 		this.products = gems;
 
 	});
@@ -19,6 +19,19 @@
 		{
 			this.tab = setTab;
 		};
+
+	});
+
+	app.controller('ReviewController', function(){
+
+		this.review = {};
+
+		this.addReview = function(product) {
+			this.review.createdOn = Date.now();
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+
 
 	});
 
